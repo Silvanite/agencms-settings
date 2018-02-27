3840b99c32e522a8489b7b19f69adc830af43e86
Marco Mark <marco@silvanite.com>
2018-02-27 21:11:32 +0000

    Additional helper method for retrieving saved settings

--- src/Settings.php
+++ src/Settings.php
@@ -11,4 +11,30 @@ class Settings extends Model
         'key',
         'value',
     ];
+
+    /**
+     * Retrieve a saved setting. Will cast to array if required.
+     *
+     * @param string $section
+     * @param string $key
+     * @param mixed $default
+     * @return string|array
+     */
+    public static function get(string $section, string $key, $default = null)
+    {
+        $setting = self::where('section', '=', $section)
+            ->where('key', '=', $key)
+            ->first();
+
+        if (!$setting) {
+            return $default;
+        }
+
+        $returnValue = json_decode($returnPlain = $setting->value, true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            $returnValue = $returnPlain;
+        }
+
+        return $returnValue;
+    }
 }
